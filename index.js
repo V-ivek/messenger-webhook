a5d23df544d4cc17685d00e3a81a57d09a9e4f0b
Vivek Selvarajan <agent47vivek@gmail.com>
2019-03-11 20:09:29 +0530

    ,

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ app.post('/webhook',(req,res)=>{
 });
 
 app.get('/webhook', (req,res)=>{
-    console.log("Incoming requests")
     let VERIFY_TOKEN = "EAAF0IK8pFhcBABwGbehhBMSjg91DbinPO4ttoZBrcucX0LaL0LGtUoc8TiIUj6UAiEfICtcr8ZC1xGzoLEVevL7iD98QUeX70VR5MBpOY8oUN2otxPkXyfPolQxVo89DJJRVrJgUBZCeJaK2Du758yT1W8UQZBhoFsuJSsGkrgZDZD";
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
